29b1b23a9f8b7532f5c967a7b832f9e8b1ee23f7
shurona <khc4669@naver.com>
2024-09-26 23:34:37 +0900

    feat: 나머지 서비스 추가

---
+++ service/user/src/main/java/com/webest/user/UserApplication.java
@@ -0,0 +1,13 @@
+package com.webest.user;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserApplication.class, args);
+	}
+
+}
