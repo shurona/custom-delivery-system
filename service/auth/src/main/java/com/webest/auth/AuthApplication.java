a64e3bc3fe083aa8271553e5a286af33fe87f0d7
shurona <khc4669@naver.com>
2024-09-27 14:48:12 +0900

    feat: 컴포넌트 스캔 범위 모든 모듈에 추가

--- service/auth/src/main/java/com/webest/auth/AuthApplication.java
+++ service/auth/src/main/java/com/webest/auth/AuthApplication.java
@@ -1,13 +1,15 @@
 package com.webest.auth;
 
+import static com.webest.web.common.CommonStaticVariable.BASE_PACKAGE;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = BASE_PACKAGE)
 public class AuthApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(AuthApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(AuthApplication.class, args);
+    }
 
 }
