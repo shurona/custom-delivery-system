6a771e6b9f1abe6ec6a701840737f95eac7590cd
suin <suin505@gmail.com>
2024-09-30 12:02:12 +0900

    feat: 카테고리 조회

--- service/store/src/main/java/com/webest/store/category/api/CategoryController.java
+++ service/store/src/main/java/com/webest/store/category/api/CategoryController.java
@@ -6,20 +6,35 @@ import com.webest.store.category.application.CategoryService;
 import com.webest.web.response.CommonResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/categories")
+@RequestMapping("/api/v1/categories")
 public class CategoryController {
     private final CategoryService categoryService;
 
+    // 카테고리 생성
     @PostMapping
     public CommonResponse<CategoryResponse> saveCategory(CreateCategoryRequest createCategoryRequest) {
          CategoryResponse categoryResponse = categoryService.saveCategory(createCategoryRequest);
         return CommonResponse.success(categoryResponse);
+    }
+
+    // 카테고리 단건 조회
+    @GetMapping("{id}")
+    public CommonResponse<CategoryResponse> getCategoryById(@PathVariable("id") Long id) {
+        CategoryResponse categoryResponse = categoryService.getCategoryById(id);
+        return CommonResponse.success(categoryResponse);
+    }
+
+    // 카테고리 전체 조회
+    @GetMapping
+    public CommonResponse<List<CategoryResponse>> getAllCategories() {
+        List<CategoryResponse> categoryResponses = categoryService.getAllCategories();
+        return CommonResponse.success(categoryResponses);
     }
 
 }
--- service/store/src/main/java/com/webest/store/category/application/CategoryService.java
+++ service/store/src/main/java/com/webest/store/category/application/CategoryService.java
@@ -7,8 +7,12 @@ import com.webest.store.category.domain.CategoryRepository;
 import com.webest.store.category.exception.CategoryErrorCode;
 import com.webest.store.category.exception.CategoryException;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @Transactional(readOnly = true)
@@ -16,6 +20,8 @@ import org.springframework.transaction.annotation.Transactional;
 public class CategoryService {
     private final CategoryRepository categoryRepository;
 
+
+    // 카테고리 생성
     @Transactional
     public CategoryResponse saveCategory(CreateCategoryRequest createCategoryRequest) {
         checkIfCategoryExistsByKey(createCategoryRequest.key());
@@ -24,10 +30,29 @@ public class CategoryService {
         return CategoryResponse.of(storeCategory);
     }
 
+    // 카테고리 단건 조회
+    public CategoryResponse getCategoryById(Long id) {
+        StoreCategory storeCategory = findCategoryById(id);
+        return CategoryResponse.of(storeCategory);
+    }
+
+    // 카테고리 전체 조회
+    public List<CategoryResponse> getAllCategories() {
+        return categoryRepository.findAll().stream()
+                .map(CategoryResponse::of)
+                .collect(Collectors.toList());
+    }
+
     // key 값으로 카테고리가 이미 존재하는지 확인하는 메서드
     private void checkIfCategoryExistsByKey(String key) {
         if (categoryRepository.existsByKey(key)) {
             throw new CategoryException(CategoryErrorCode.CATEGORY_ALREADY_EXISTS);
         }
+    }
+
+    // ID로 카테고리를 찾는 공통 메서드
+    private StoreCategory findCategoryById(Long id) {
+        return categoryRepository.findById(id)
+                .orElseThrow(() -> new CategoryException(CategoryErrorCode.CATEGORY_NOT_FOUND));
     }
 }
