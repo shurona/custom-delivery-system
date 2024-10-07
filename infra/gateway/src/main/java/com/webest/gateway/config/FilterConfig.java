03f74348e8d15c068db3d38444f98d5b7c21ee13
t2023-m0111 <qowhxk@naver.com>
2024-10-07 11:24:19 +0900

    EndPoint Filter Update

--- infra/gateway/src/main/java/com/webest/gateway/config/FilterConfig.java
+++ infra/gateway/src/main/java/com/webest/gateway/config/FilterConfig.java
@@ -40,7 +40,65 @@ public class FilterConfig {
                                 .filter(authorizationHeaderFilter.apply(new AuthorizationHeaderFilter.Config())))  // 필터 팩토리로 필터 생성
                         .uri("lb://USER-SERVICE"))
 
+                .route("user-get-all-route", r -> r.path("/api/v1/orders/**")
+                        .and()
+                        .method("GET","POST","PATCH","DELETE")
+                        .filters(f -> f
+                                .removeRequestHeader("Cookie")
+                                .filter(authorizationHeaderFilter.apply(new AuthorizationHeaderFilter.Config())))  // 필터 팩토리로 필터 생성
+                        .uri("lb://ORDER-SERVICE"))
 
+                .route("user-get-all-route", r -> r.path("/api/v1/deliveries/**")
+                        .and()
+                        .method("GET","POST","PATCH","DELETE")
+                        .filters(f -> f
+                                .removeRequestHeader("Cookie")
+                                .filter(authorizationHeaderFilter.apply(new AuthorizationHeaderFilter.Config())))  // 필터 팩토리로 필터 생성
+                        .uri("lb://DELIVERY-SERVICE"))
+
+
+                .route("user-get-all-route", r -> r.path("/api/v1/stores/**")
+                        .and()
+                        .method("GET","POST","PATCH","DELETE")
+                        .filters(f -> f
+                                .removeRequestHeader("Cookie")
+                                .filter(authorizationHeaderFilter.apply(new AuthorizationHeaderFilter.Config())))  // 필터 팩토리로 필터 생성
+                        .uri("lb://STORE-SERVICE"))
+
+
+                .route("user-get-all-route", r -> r.path("/api/v1/categories/**")
+                        .and()
+                        .method("GET","POST","PATCH","DELETE")
+                        .filters(f -> f
+                                .removeRequestHeader("Cookie")
+                                .filter(authorizationHeaderFilter.apply(new AuthorizationHeaderFilter.Config())))  // 필터 팩토리로 필터 생성
+                        .uri("lb://STORE-SERVICE"))
+
+                .route("user-get-all-route", r -> r.path("/api/v1/products/**")
+                        .and()
+                        .method("GET","POST","PATCH","DELETE")
+                        .filters(f -> f
+                                .removeRequestHeader("Cookie")
+                                .filter(authorizationHeaderFilter.apply(new AuthorizationHeaderFilter.Config())))  // 필터 팩토리로 필터 생성
+                        .uri("lb://PRODUCT-SERVICE"))
+
+                .route("user-get-all-route", r -> r.path("/api/v1/coupons/**")
+                        .and()
+                        .method("GET","POST","PATCH","DELETE")
+                        .filters(f -> f
+                                .removeRequestHeader("Cookie")
+                                .filter(authorizationHeaderFilter.apply(new AuthorizationHeaderFilter.Config())))  // 필터 팩토리로 필터 생성
+                        .uri("lb://COUPON-SERVICE"))
+
+                .route("user-get-all-route", r -> r.path("/api/v1/events/**")
+                        .and()
+                        .method("GET","POST","PATCH","DELETE")
+                        .filters(f -> f
+                                .removeRequestHeader("Cookie")
+                                .filter(authorizationHeaderFilter.apply(new AuthorizationHeaderFilter.Config())))  // 필터 팩토리로 필터 생성
+                        .uri("lb://COUPON-SERVICE"))
                 .build();
+
+
     }
 }
