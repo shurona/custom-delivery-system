c72f99a34bfab69e770e5f74f9cc5b6c1112dc63
suin <suin505@gmail.com>
2024-10-13 14:15:01 +0900

    chore: 코드 정리

--- service/store/src/main/java/com/webest/store/store/application/context/StoreStrategyContext.java
+++ service/store/src/main/java/com/webest/store/store/application/context/StoreStrategyContext.java
@@ -3,8 +3,6 @@ package com.webest.store.store.application.context;
 import com.webest.store.store.domain.strategy.StoreStrategy;
 import com.webest.store.store.exception.StoreErrorCode;
 import com.webest.store.store.exception.StoreException;
-import com.webest.store.store.infrastructure.user.UserClient;
-import com.webest.store.store.infrastructure.user.dto.UserResponse;
 import com.webest.store.store.presentation.dto.StoreResponse;
 import com.webest.web.common.UserRole;
 import lombok.RequiredArgsConstructor;
@@ -17,12 +15,19 @@ import java.util.List;
 public class StoreStrategyContext {
     private final List<StoreStrategy> storeStrategies;
 
-    public List<StoreResponse> getAllStores(String userId, UserRole role) {
+    public List<StoreResponse> getDeliveryStores(String userId, UserRole role) {
         return storeStrategies.stream()
                 .filter(storeStrategy -> storeStrategy.checkUserRole(role))
                 .findAny()
                 .orElseThrow(() -> new StoreException(StoreErrorCode.UNAUTHORIZED_ACCESS))
                 .getDeliveryStores(userId);
+    }
 
+    public List<StoreResponse> getTakeOutStores(String userId, UserRole role) {
+        return storeStrategies.stream()
+                .filter(storeStrategy -> storeStrategy.checkUserRole(role))
+                .findAny()
+                .orElseThrow(() -> new StoreException(StoreErrorCode.UNAUTHORIZED_ACCESS))
+                .getTakeOutStores(userId);
     }
 }
