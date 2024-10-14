7bd88988f18386d4148532ac967214d3275fe6fd
t2023-m0111 <qowhxk@naver.com>
2024-10-14 13:37:56 +0900

    RedisTemplate 구조 중복 오류 해결

--- service/auth/src/main/java/com/webest/auth/infrastructure/redis/RedisUtil.java
+++ service/auth/src/main/java/com/webest/auth/infrastructure/redis/RedisUtil.java
@@ -2,47 +2,49 @@ package com.webest.auth.infrastructure.redis;
 
 import com.webest.auth.domain.model.vo.RefreshTokenDto;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 
 import java.time.Duration;
 import java.util.concurrent.TimeUnit;
 
-@Service
-@RequiredArgsConstructor
+@Component
 public class RedisUtil {
 
-    private final RedisTemplate<String, Object> redisTemplate;
+    private final RedisTemplate<String, RefreshTokenDto> refreshTokenRedisTemplate;
+    private final RedisTemplate<String, String> stringRedisTemplate;
 
     private static final String TOKEN_PREFIX = "refresh_token:";
 
+    public RedisUtil(@Qualifier("customStringRedisTemplate") RedisTemplate<String, String> stringRedisTemplate,
+                     @Qualifier("customRefreshTokenRedisTemplate") RedisTemplate<String, RefreshTokenDto> refreshTokenRedisTemplate) {
+        this.stringRedisTemplate = stringRedisTemplate;
+        this.refreshTokenRedisTemplate = refreshTokenRedisTemplate;
+    }
+
     public Object getData(String key){//지정된 키(key)에 해당하는 데이터를 Redis에서 가져오는 메서드
-        redisTemplate.opsForValue().get(key);
-        return redisTemplate.opsForValue().get(key);
+        stringRedisTemplate.opsForValue().get(key);
+        return stringRedisTemplate.opsForValue().get(key);
     }
-    public void setData(String key,String value){//지정된 키(key)에 값을 저장하는 메서드
-        redisTemplate.opsForValue().set(key,value);
-    }
+
     // email 전용
     public void setDataExpire(String key,String value,long duration){//지정된 키(key)에 값을 저장하고, 지정된 시간(duration) 후에 데이터가 만료되도록 설정하는 메서드
         Duration expireDuration=Duration.ofSeconds(duration);
-        redisTemplate.opsForValue().set(key,value,expireDuration);
+        stringRedisTemplate.opsForValue().set(key,value,expireDuration);
     }
 
     // RefreshToken 전용
     public void setDataRefreshToken(RefreshTokenDto dto){//key - 유저 데이터,value - refreshToken 지정된 시간(duration) 후에 데이터가 만료되도록 설정하는 메서드
         String key = TOKEN_PREFIX + dto.userId();
         // 만료시간 1시간
-        redisTemplate.opsForValue().set(key,dto,1, TimeUnit.HOURS);
+        refreshTokenRedisTemplate.opsForValue().set(key,dto,1, TimeUnit.HOURS);
     }
 
     // RefreshToken 전용
     public RefreshTokenDto getRefreshToken(String userId) {
         String key = TOKEN_PREFIX + userId;
-        return (RefreshTokenDto) redisTemplate.opsForValue().get(key);
-    }
-
-    public void deleteData(String key){//지정된 키(key)에 해당하는 데이터를 Redis에서 삭제하는 메서드
-        redisTemplate.delete(key);
+        return (RefreshTokenDto) refreshTokenRedisTemplate.opsForValue().get(key);
     }
 }
