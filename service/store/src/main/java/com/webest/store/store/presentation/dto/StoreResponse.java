eb8926131f172870f9e32bd04981719c55d04dcb
suin <suin505@gmail.com>
2024-10-14 11:30:30 +0900

    fix: Store의 ownerId Long -> String으로 변경

--- service/store/src/main/java/com/webest/store/store/presentation/dto/StoreResponse.java
+++ service/store/src/main/java/com/webest/store/store/presentation/dto/StoreResponse.java
@@ -10,7 +10,7 @@ import java.util.List;
 public record StoreResponse(
         Long id,
         String name,
-        Long ownerId,
+        String ownerId,
         Long categoryId,
         Integer preparationTime,
         Double minimumOrderAmount,
