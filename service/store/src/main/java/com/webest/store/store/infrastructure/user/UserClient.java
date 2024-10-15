a83202f6156a6fcc9260ef26d5725e1578c733a1
shurona <khc4669@naver.com>
2024-10-15 18:29:52 +0900

    fix: userClient local 제거

--- service/store/src/main/java/com/webest/store/store/infrastructure/user/UserClient.java
+++ service/store/src/main/java/com/webest/store/store/infrastructure/user/UserClient.java
@@ -6,9 +6,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 
-@FeignClient(name = "user-service",
-            url = "http://127.0.0.1:19092"
-)
+@FeignClient(name = "user-service")
 public interface UserClient {
     @GetMapping("/api/users/myPage")
     CommonResponse<UserResponse> getUser(
