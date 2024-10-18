26c761dc924ba11e59b058c6acb27571a41b268b
suin <suin505@gmail.com>
2024-10-18 14:39:33 +0900

    fix: CreateStoreRequest validation 수정

--- service/store/src/main/java/com/webest/store/store/presentation/dto/CreateStoreRequest.java
+++ service/store/src/main/java/com/webest/store/store/presentation/dto/CreateStoreRequest.java
@@ -9,13 +9,13 @@ public record CreateStoreRequest(
         @NotBlank(message = "상점 이름은 필수 입력 항목입니다.")
         String name,
         @NotNull(message = "카테고리 ID를 입력해주세요.")
+        @Positive(message = "카테고리 ID는 양수여야 합니다.")
         Long categoryId,
         @Min(value = 1, message = "준비 시간은 최소 1분 이상이어야 합니다.")
         Integer preparationTime,
         @NotNull(message = "최소 주문 금액을 입력해주세요.")
         @DecimalMin(value = "0.0", inclusive = false, message = "최소 주문 금액은 0보다 커야 합니다.")
         Double minimumOrderAmount,
-        @NotBlank(message = "")
         @Pattern(regexp = "^\\d{2,3}-\\d{3,4}-\\d{4}$", message = "전화번호를 xx-xxxx-xxxx 형식으로입력해주세요.")
         String phone,
         LocalTime openTime,
