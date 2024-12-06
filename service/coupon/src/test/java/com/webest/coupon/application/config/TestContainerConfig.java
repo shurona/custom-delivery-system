50558abd7111963145f5101da0328cdabf9e987f
shurona <khc4669@naver.com>
2024-12-06 14:55:25 +0900

    feat: 카프카 testcontainer 추가

--- service/coupon/src/test/java/com/webest/coupon/application/config/TestContainerConfig.java
+++ service/coupon/src/test/java/com/webest/coupon/application/config/TestContainerConfig.java
@@ -10,16 +10,52 @@ import org.testcontainers.utility.DockerImageName;
 public class TestContainerConfig implements BeforeAllCallback {
 
     public static final String REDIS_IMAGE = "redis:7.4.0-alpine";
+    public static final String KAFKA_IMAGE = "confluentinc/cp-kafka:7.7.1.arm64";
+
     public static final Integer port = 6379;
+    public static final Integer kafkaPort = 9092;
+    //    private static final KafkaContainer kafka = new KafkaContainer(
+//        DockerImageName.parse(KAFKA_IMAGE));
     public static GenericContainer<?> redis;
+    public static GenericContainer<?> kafka;
+
+//    static {
+//        kafka.start();
+//    }
+//
+//    @DynamicPropertySource
+//    static void setKafkaProperties(DynamicPropertyRegistry registry) {
+//        registry.add("spring.kafka.bootstrap-servers", kafka::getBootstrapServers);
+//    }
 
     @Override
     public void beforeAll(ExtensionContext context) throws Exception {
+        // redis
+        startRedisContainer();
+
+        // kafka
+        startKafkaContainer();
+    }
+
+    private void startRedisContainer() {
         redis = new GenericContainer(
             DockerImageName.parse(REDIS_IMAGE)).withExposedPorts(port).withReuse(true);
 
         redis.start();
         System.setProperty("spring.data.redis.host", redis.getHost());
         System.setProperty("spring.data.redis.port", String.valueOf(redis.getMappedPort(port)));
+
     }
+
+    private void startKafkaContainer() {
+        kafka = new GenericContainer(
+            DockerImageName.parse(KAFKA_IMAGE)).withExposedPorts(kafkaPort).withReuse(true);
+
+        kafka.start();
+        System.setProperty("spring.data.kafka.host", kafka.getHost());
+        System.setProperty("spring.data.kafka.port",
+            String.valueOf(kafka.getMappedPort(kafkaPort)));
+    }
+
+
 }
