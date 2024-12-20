487de04acc8a1cc755ef03d48d9e088b00f30c21
shurona <khc4669@naver.com>
2024-12-20 18:14:01 +0900

    fix: orderService formatter 적용

--- service/order/src/main/java/com/webest/order/application/service/OrderService.java
+++ service/order/src/main/java/com/webest/order/application/service/OrderService.java
@@ -1,7 +1,6 @@
 package com.webest.order.application.service;
 
 import com.webest.order.application.dtos.OrderDto;
-import com.webest.order.application.dtos.OrderProductDto;
 import com.webest.order.application.dtos.OrderSearchDto;
 import com.webest.order.application.dtos.OrderUpdateDto;
 import com.webest.order.domain.events.OrderPaymentCompletedEvent;
@@ -11,25 +10,25 @@ import com.webest.order.domain.model.Order;
 import com.webest.order.domain.model.OrderProduct;
 import com.webest.order.domain.model.OrderStatus;
 import com.webest.order.domain.repository.order.OrderRepository;
-import com.webest.order.domain.service.*;
+import com.webest.order.domain.service.CouponService;
+import com.webest.order.domain.service.OrderCouponDomainService;
+import com.webest.order.domain.service.OrderProductDomainService;
+import com.webest.order.domain.service.StoreService;
+import com.webest.order.domain.service.UserService;
 import com.webest.order.domain.validator.OrderProductValidator;
 import com.webest.order.infrastructure.client.coupon.dto.CouponByUserResponseDto;
 import com.webest.order.infrastructure.client.store.dto.ProductResponse;
-import com.webest.order.infrastructure.client.user.UserClient;
-import com.webest.order.presentation.response.OrderResponse;
 import com.webest.order.infrastructure.client.store.dto.StoreResponse;
 import com.webest.order.infrastructure.client.user.dto.UserResponse;
+import com.webest.order.presentation.response.OrderResponse;
 import com.webest.web.common.UserRole;
 import jakarta.transaction.Transactional;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
-
-import java.util.List;
-import java.util.Map;
-import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
@@ -38,8 +37,6 @@ public class OrderService {
     private final OrderRepository orderRepository;
 
     private final OrderEventService orderEventService;
-
-    private final OrderProductService orderProductService;
 
     private final UserService userService;
 
@@ -55,12 +52,12 @@ public class OrderService {
 
     /**
      * 주문 생성
+     *
      * @param request 주문 생성에 필요한 정보를 담은 OrderDto
      * @return 저장된 주문 데이터를 담은 OrderResponse
      */
     @Transactional
     public OrderResponse createOrder(String userId, UserRole userRole, OrderDto request) {
-
 
         // 유저 정보 가져오기 (유저 주소값)
         UserResponse userResponse = userService.getUser(userId);
@@ -69,43 +66,44 @@ public class OrderService {
         StoreResponse storeResponse = storeService.getStore(request.storeId());
 
         // 상점아이디로 존재하는 상품 검색
-        Page<ProductResponse> productResponse = storeService.getProductsByStore(request.storeId(), Pageable.unpaged());
+        Page<ProductResponse> productResponse = storeService.getProductsByStore(request.storeId(),
+            Pageable.unpaged());
 
         // 상점에 포함된 상품만 요청가능하게 validate
         orderProductValidator.validateProductExistence(productResponse, request);
 
         // 주문 상품 create
         List<OrderProduct> orderProducts =
-                orderProductDomainService.createOrderProduct(request);
+            orderProductDomainService.createOrderProduct(request);
 
         // coupon 적용
         List<CouponByUserResponseDto> couponByUserResponseDtoList =
-                couponService.findCouponByUserId(userId, userRole, false);
+            couponService.findCouponByUserId(userId, userRole, false);
 
         // 사용된 쿠폰 validation
         orderCouponDomainService.validateCouponUsage(
-                request.couponId(), couponByUserResponseDtoList
+            request.couponId(), couponByUserResponseDtoList
         );
 
         Order order = Order.create(
-                request.storeId(),
-                request.paymentId(),
-                request.couponId(),
-                userId,
-                request.orderStatus(),
-                request.isRequest(),
-                request.requestsToStore(),
-                request.requestsToRider(),
-                userResponse.addressCode(),
-                userResponse.detailAddress(),
-                storeResponse.storeAddress().addressCode(),
-                storeResponse.storeAddress().detailAddress(),
-                request.totalQuantity(),
-                request.totalProductPrice(),
-                request.couponAppliedAmount(),
-                request.deliveryTipAmount(),
-                request.totalPaymentPrice(),
-                orderProducts
+            request.storeId(),
+            request.paymentId(),
+            request.couponId(),
+            userId,
+            request.orderStatus(),
+            request.isRequest(),
+            request.requestsToStore(),
+            request.requestsToRider(),
+            userResponse.addressCode(),
+            userResponse.detailAddress(),
+            storeResponse.storeAddress().addressCode(),
+            storeResponse.storeAddress().detailAddress(),
+            request.totalQuantity(),
+            request.totalProductPrice(),
+            request.couponAppliedAmount(),
+            request.deliveryTipAmount(),
+            request.totalPaymentPrice(),
+            orderProducts
         );
 
         // 주문 저장
@@ -125,7 +123,7 @@ public class OrderService {
     public void paymentCompleteOrder(Long orderId) {
 
         Order order = orderRepository.findById(orderId)
-                .orElseThrow(() -> new OrderException(ErrorCode.ORDER_NOT_FOUND));
+            .orElseThrow(() -> new OrderException(ErrorCode.ORDER_NOT_FOUND));
 
         OrderPaymentCompletedEvent paymentCompletedEvent = order.paymentCompletedEvent();
 
@@ -137,17 +135,19 @@ public class OrderService {
 
     /**
      * 주문 단건 조회
+     *
      * @param orderId 조회할 주문의 고유 ID (Long)
      * @return 조회된 주문 데이터를 담은 OrderResponse
      */
     @Transactional
     public OrderResponse getOrder(Long orderId) {
         return OrderResponse.of(orderRepository.findById(orderId)
-                .orElseThrow(() -> new OrderException(ErrorCode.ORDER_NOT_FOUND)));
+            .orElseThrow(() -> new OrderException(ErrorCode.ORDER_NOT_FOUND)));
     }
 
     /**
      * 주문 전체 조회
+     *
      * @return 모든 주문을 담고 있는 OrderResponse 리스트
      */
     @Transactional
@@ -160,33 +160,34 @@ public class OrderService {
 
     /**
      * 주문 수정
+     *
      * @param orderId 주문을 업데이트하기 위한 ID
      * @param request 주문 업데이트에 필요한 데이터
      * @return 수정된 주문 데이터를 담은 OrderResponse
      */
     @Transactional
-    public OrderResponse updateOrder(String userId, UserRole userRole, Long orderId, OrderUpdateDto request) {
-
+    public OrderResponse updateOrder(String userId, UserRole userRole, Long orderId,
+        OrderUpdateDto request) {
 
         return orderRepository.findById(orderId).map(order -> {
             order.update(
-                    request.storeId(),
-                    request.paymentId(),
-                    request.couponId(),
-                    userId,
-                    request.orderStatus(),
-                    request.isRequest(),
-                    request.requestsToStore(),
-                    request.requestsToRider(),
-                    request.storeAddressCode(),
-                    request.storeDetailAddress(),
-                    request.arrivalAddressCode(),
-                    request.arrivalDetailAddress(),
-                    request.totalQuantity(),
-                    request.totalProductPrice(),
-                    request.couponAppliedAmount(),
-                    request.deliveryTipAmount(),
-                    request.totalPaymentPrice()
+                request.storeId(),
+                request.paymentId(),
+                request.couponId(),
+                userId,
+                request.orderStatus(),
+                request.isRequest(),
+                request.requestsToStore(),
+                request.requestsToRider(),
+                request.storeAddressCode(),
+                request.storeDetailAddress(),
+                request.arrivalAddressCode(),
+                request.arrivalDetailAddress(),
+                request.totalQuantity(),
+                request.totalProductPrice(),
+                request.couponAppliedAmount(),
+                request.deliveryTipAmount(),
+                request.totalPaymentPrice()
             );
             orderEventService.publishOrderUpdatedEvent(order.updatedEvent());
             return OrderResponse.of(order);
@@ -195,6 +196,7 @@ public class OrderService {
 
     /**
      * 주문 삭제
+     *
      * @param orderId 삭제할 주문의 ID
      */
     @Transactional
@@ -212,17 +214,20 @@ public class OrderService {
 
     /**
      * 주문 검색
+     *
      * @param userId 유저 아이디 값
      */
     @Transactional
-    public Page<OrderResponse> searchOrders(String userId, UserRole userRole, OrderSearchDto request, PageRequest pageRequest) {
+    public Page<OrderResponse> searchOrders(String userId, UserRole userRole,
+        OrderSearchDto request, PageRequest pageRequest) {
 
         return orderRepository.searchOrders(request, pageRequest)
-                .map(OrderResponse::of);
+            .map(OrderResponse::of);
     }
 
     /**
      * 주문 확인 (주문 상태값이 CONFIRMING_ORDER -> PREPARING 변경)
+     *
      * @param orderId 주문 아이디 값
      */
     @Transactional
@@ -231,7 +236,7 @@ public class OrderService {
 
             order.preparing();
 
-            if(order.getOrderStatus() == OrderStatus.CONFIRMING_ORDER) {
+            if (order.getOrderStatus() == OrderStatus.CONFIRMING_ORDER) {
                 throw new OrderException(ErrorCode.ALREADY_CANCELLED_ORDER);
             }
 
@@ -243,36 +248,37 @@ public class OrderService {
 
     /**
      * 주문 요청(배달)
+     *
      * @param orderId 배달 요청할 주문의 ID
      */
     @Transactional
     public OrderResponse requestOrder(String userId, UserRole userRole, Long orderId) {
 
-       return orderRepository.findById(orderId).map(order -> {
+        return orderRepository.findById(orderId).map(order -> {
 
-           // 주문 상태가 PREPARING이 아닐때 배달요청 불가
-           if (order.getOrderStatus() != OrderStatus.PREPARING) {
-               throw new OrderException(ErrorCode.INVALID_ORDER_STATUS);
-           }
+            // 주문 상태가 PREPARING이 아닐때 배달요청 불가
+            if (order.getOrderStatus() != OrderStatus.PREPARING) {
+                throw new OrderException(ErrorCode.INVALID_ORDER_STATUS);
+            }
 
-           order.requestOrder();
+            order.requestOrder();
 
-           orderEventService.publishOrderRequestEvent(order.requestedEvent());
+            orderEventService.publishOrderRequestEvent(order.requestedEvent());
 
-           return OrderResponse.of(order);
+            return OrderResponse.of(order);
         }).orElseThrow(() -> new OrderException(ErrorCode.ORDER_NOT_FOUND));
 
     }
 
     /**
      * 주문 완료(배달완료 되었을때 이벤트를 받아서 처리)
+     *
      * @param orderId 주문 완료 주문의 ID
      */
     @Transactional
     public OrderResponse completeOrder(Long orderId) {
 
         return orderRepository.findById(orderId).map(order -> {
-
 
             // payment 기능 완료 될 시 paymentStatus에 따라 주문취소 혹은 주문완료 구현
 
@@ -287,6 +293,7 @@ public class OrderService {
 
     /**
      * 주문 취소
+     *
      * @param orderId 주문 아이디 값
      */
     @Transactional
@@ -318,7 +325,6 @@ public class OrderService {
         }).orElseThrow(() -> new OrderException(ErrorCode.ORDER_NOT_FOUND));
 
     }
-
 
 
 }
