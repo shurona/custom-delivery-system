0d2f8469a6c180792f44b479da613a745316d221
shurona <khc4669@naver.com>
2024-12-25 17:33:18 +0900

    fix: GeoOperation formatter 적용

--- service/store/src/main/java/com/webest/store/store/application/GeoOperation.java
+++ service/store/src/main/java/com/webest/store/store/application/GeoOperation.java
@@ -1,15 +1,18 @@
 package com.webest.store.store.application;
 
 import jakarta.annotation.Resource;
-import org.springframework.data.geo.*;
+import java.util.List;
+import java.util.stream.Collectors;
+import org.springframework.data.geo.Circle;
+import org.springframework.data.geo.Distance;
+import org.springframework.data.geo.GeoResult;
+import org.springframework.data.geo.GeoResults;
+import org.springframework.data.geo.Point;
 import org.springframework.data.redis.connection.RedisGeoCommands;
 import org.springframework.data.redis.core.GeoOperations;
 import org.springframework.data.redis.domain.geo.GeoLocation;
 import org.springframework.data.redis.domain.geo.Metrics;
 import org.springframework.stereotype.Component;
-
-import java.util.List;
-import java.util.stream.Collectors;
 
 @Component
 public class GeoOperation {
@@ -26,10 +29,12 @@ public class GeoOperation {
 
     public List<String> findNearByStores(Double x, Double y, Double radius) {
         Circle circle = new Circle(new Point(x, y), new Distance(radius, Metrics.KILOMETERS));
-        GeoResults<RedisGeoCommands.GeoLocation<String>> res = geoOperations.radius(STORE_CACHE_PREFIX, circle);
+        GeoResults<RedisGeoCommands.GeoLocation<String>> res = geoOperations.radius(
+            STORE_CACHE_PREFIX, circle);
         return res.getContent().stream()
-                .map(GeoResult::getContent)
-                .map(GeoLocation::getName)
-                .collect(Collectors.toList());
+            .map(GeoResult::getContent)
+            .map(GeoLocation::getName)
+            .collect(Collectors.toList());
     }
+
 }
